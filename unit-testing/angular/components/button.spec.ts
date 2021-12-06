8dc68eac3ebd522ce18bd59ce4490492f26756a0
Jean-Yves Moyen <jym@siteimprove.com>
2021-12-06 10:30:17 +0100

    Get Vue test to work

--- unit-testing/angular/components/button.spec.ts
+++ unit-testing/angular/components/button.spec.ts
@@ -5,8 +5,9 @@ import { ButtonComponent } from "./button";
 
 let fixture: ComponentFixture<ButtonComponent>;
 
-// window.matchMedia is not currently implemented by Jest (?), so we need to mock
-// it.
+// window.matchMedia is not currently implemented by JSDOM, used by Jest, so
+// we need to mock it.
+// see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 // For the purpose of this test, we actually don't care about the result (they
 // are not actually used), so we are OK with a mock that always answer `false`.
 window.matchMedia =
--- unit-testing/vue/components/button.spec.ts
+++ unit-testing/vue/components/button.spec.ts
@@ -4,6 +4,19 @@ import { mount } from "@vue/test-utils";
 
 import { Button } from "./button";
 
+// window.matchMedia is not currently implemented by JSDOM, used by Jest, so
+// we need to mock it.
+// see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+// For the purpose of this test, we actually don't care about the result (they
+// are not actually used), so we are OK with a mock that always answer `false`.
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+    };
+  };
+
 it("should be accessible", async () => {
-  await expect(mount(Button)).toBeAccessible();
+  await expect(mount(Button)).not.toBeAccessible();
 });
