e8375a8f57f856c034842edc28a169e4dc985b9f
Jean-Yves Moyen <jym@siteimprove.com>
2021-12-13 13:34:24 +0100

    Fix import of rules

--- unit-testing/angular/setup.ts
+++ unit-testing/angular/setup.ts
@@ -2,11 +2,14 @@ import { Angular } from "@siteimprove/alfa-angular";
 
 import * as alfa from "@siteimprove/alfa-jest";
 
-// Selecting a rule that applies to buttons
-import R12 from "@siteimprove/alfa-rules";
+// Only selecting a rule that apply to buttons.
+import { Rules } from "@siteimprove/alfa-rules";
+const R12 = Rules.get("R12").get();
 
 import { persist } from "../../common/persist";
 
-alfa.Jest.createPlugin((value: Angular.Type) => Angular.toPage(value), R12, [
-  persist(() => "outcomes/button.spec.json"),
-]);
+alfa.Jest.createPlugin(
+  (value: Angular.Type) => Angular.toPage(value),
+  [R12],
+  [persist(() => "outcomes/button.spec.json")]
+);
--- unit-testing/vue/setup.ts
+++ unit-testing/vue/setup.ts
@@ -1,10 +1,15 @@
 import { Vue } from "@siteimprove/alfa-vue";
 
 import * as alfa from "@siteimprove/alfa-jest";
-import rules from "@siteimprove/alfa-rules";
+
+// Only selecting a rule that apply to buttons.
+import { Rules } from "@siteimprove/alfa-rules";
+const R12 = Rules.get("R12").get();
 
 import { persist } from "../../common/persist";
 
-alfa.Jest.createPlugin((value: Vue.Type) => Vue.toPage(value), rules, [
-  persist(() => "outcomes/button.spec.json"),
-]);
+alfa.Jest.createPlugin(
+  (value: Vue.Type) => Vue.toPage(value),
+  [R12],
+  [persist(() => "outcomes/button.spec.json")]
+);
