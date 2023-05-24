be7749fc8475318c83cfa3245d9687ed0abf4fc0
Jean-Yves Moyen <jym@siteimprove.com>
2023-05-24 13:04:23 +0200

    Desactivate breaking angular tests

--- unit-testing/vue/setup.ts
+++ unit-testing/vue/setup.ts
@@ -2,6 +2,10 @@ import { Vue } from "@siteimprove/alfa-vue";
 
 import * as alfa from "@siteimprove/alfa-jest";
 
+// Not sure why this is needed, but it fixes a missing TextEncoder problem.
+// It might be something that needs to be fixed in alfa-jest, but we seem to not
+// be the only ones with this problem.
+// See https://github.com/inrupt/solid-client-authn-js/issues/1676#issuecomment-917016646
 import { TextEncoder } from "util";
 global.TextEncoder = TextEncoder;
 
