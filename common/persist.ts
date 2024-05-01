a359cb576421782991a363945f5687c289fabc63
Jean-Yves Moyen <jym@siteimprove.com>
2024-05-01 16:29:58 +0200

    Reformat

--- common/persist.ts
+++ common/persist.ts
@@ -1,16 +1,15 @@
 /// <reference types="node" />
 
-import * as fs from "fs";
-import * as path from "path";
-
 import { Question } from "@siteimprove/alfa-act";
 import { Handler } from "@siteimprove/alfa-assert";
 import { Formatter } from "@siteimprove/alfa-formatter";
+import earl from "@siteimprove/alfa-formatter-earl";
 import { Future } from "@siteimprove/alfa-future";
 import { Hashable } from "@siteimprove/alfa-hash";
 import { Mapper } from "@siteimprove/alfa-mapper";
 
-import earl from "@siteimprove/alfa-formatter-earl";
+import * as fs from "fs";
+import * as path from "path";
 
 export function persist<I, T extends Hashable, Q extends Question.Metadata, S>(
   output: Mapper<I, string>,
@@ -22,7 +21,7 @@ export function persist<I, T extends Hashable, Q extends Question.Metadata, S>(
       const dir = path.dirname(file);
 
       fs.mkdirSync(dir, { recursive: true });
-      fs.writeFileSync(file, format(input, rules, outcomes) + "\n");
+      fs.writeFileSync(file, format(input, rules, [...outcomes]) + "\n");
 
       return `${message}, see the full report at ${file}`;
     });
