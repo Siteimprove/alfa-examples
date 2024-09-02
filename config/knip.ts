cfddf8297dbbd9b5811bd5748e944eb2ad3495d4
Jean-Yves Moyen <jym@siteimprove.com>
2024-09-02 11:10:14 +0200

    Add basic knip config

---
+++ config/knip.ts
@@ -0,0 +1,44 @@
+import type { KnipConfig } from "knip";
+
+const entry = ["test/**/*.spec.ts", "test/**/*.spec.tsx"];
+const project = ["**/*.ts", "**/*.tsx"];
+
+const config: KnipConfig = {
+  workspaces: {
+    ".": {
+      entry: ["common/*.ts"],
+      project: ["common/**/*.ts"],
+      ignoreDependencies: ["prettier"],
+    },
+    "custom-testing/*": { entry, project },
+    "custom-testing/crawling": { entry: ["crawling.ts"], project },
+    "custom-testing/measuring-performances": {
+      entry: ["performance.ts"],
+      project,
+    },
+    "custom-testing/scraping": { entry: ["scraping.ts"], project },
+    "end-to-end-testing/*": { entry, project },
+    "unit-testing/*": { entry, project },
+    // "packages/alfa-cascade": {
+    //   entry,
+    //   project,
+    //   // For some reason, knip doesn't detect that Bucket is used in the test
+    //   // file and needs to be exported for that.
+    //   ignore: ["src/ancestor-filter.ts"],
+    // },
+    // "packages/alfa-device": { entry: [...entry, "src/native.ts!"], project },
+    // "packages/alfa-dom": {
+    //   entry: [
+    //     ...entry,
+    //     "src/h.ts!",
+    //     "src/jsx.ts!",
+    //     "src/jsx-runtime.ts!",
+    //     "src/native.ts!",
+    //   ],
+    //   project,
+    // },
+    // "packages/alfa-web": { entry: [...entry, "src/native.ts!"], project },
+  },
+};
+
+export default config;
