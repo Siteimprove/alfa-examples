994a1d2b41282d9c642731a55f26d226e2082b4a
Jean-Yves Moyen <jym@siteimprove.com>
2024-09-06 14:52:48 +0200

    Workaround missing crypto in JSDOM

--- unit-testing/vue/setup.ts
+++ unit-testing/vue/setup.ts
@@ -7,10 +7,13 @@ import * as alfa from "@siteimprove/alfa-jest";
 import { Rules } from "@siteimprove/alfa-rules";
 const R12 = Rules.get("R12").getUnsafe();
 
-import { persist } from "common/persist";
+// import { persist } from "common/persist";
 
 alfa.Jest.createPlugin(
   (value: Vue.Type) => Future.from(Vue.toPage(value)),
   [R12],
-  [persist(() => "outcomes/button.spec.json")]
+  // For some reason, jsonld isn't included correctly in the JSDOM environment.
+  // Since we do not officially support Vue integration, we just skip the
+  // persister on this test.
+  []
 );
