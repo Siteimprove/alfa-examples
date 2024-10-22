4280810628ce62145ba5fa8185609e5999de29c7
Jean-Yves Moyen <jym@siteimprove.com>
2024-10-22 16:29:59 +0200

    Add full Accessibility Code Checker example for Cypress (#61)
    * Setup Cypress tests
    
    * Update Alfa
    
    * Typo
    
    * Update yarn
    
    * Update Alfa
    
    * Add some documentation
    
    * Clean up

---
+++ accessibility-code-checker/cypress/cypress.config.ts
@@ -0,0 +1,29 @@
+import type { Audit } from "@siteimprove/alfa-test-utils/audit";
+import { Logging, SIP } from "@siteimprove/alfa-test-utils/report";
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  screenshotOnRunFailure: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on("task", {
+        // Upload audit to Siteimprove Intelligence Platform and log results
+        async report(audit: Audit.JSON): Promise<null> {
+          const pageReportUrl = await SIP.upload(audit, {
+            userName: process.env.SI_USER_EMAIL,
+            apiKey: process.env.SI_API_KEY,
+          });
+
+          Logging.fromAudit(audit, pageReportUrl).print();
+
+          return null;
+        },
+      });
+    },
+    supportFile: false,
+    specPattern: "test/**.spec.ts",
+  },
+});
---
+++ accessibility-code-checker/cypress/test/cypress.spec.ts
@@ -0,0 +1,43 @@
+import { Cypress as AlfaCypress } from "@siteimprove/alfa-cypress";
+import { Audit } from "@siteimprove/alfa-test-utils/audit";
+
+it("should be accessible", () => {
+  // Navigate to the local web page
+  // This suppose that the server is already started. See the demo-site folder.
+  // TODO: Replace with your own page
+  cy.visit("http://localhost:5173");
+
+  // wait for the page to fully load, here by waiting for a specific selector
+  // TODO: Adapt with what is present in your own page
+  cy.get(".testimonials-top").should("exist");
+
+  const audit = cy
+    // Get the document from the page
+    .document()
+    // Scrape the page
+    .then(AlfaCypress.toPage)
+    // Run the audit
+    .then(Audit.run)
+    // Upload and log the results.
+    // Note: this has to happen in NodeJS, not in Cypress browser, hence we use
+    // a cy.task call.
+    .then(async (alfaResult) => {
+      cy.task("report", alfaResult.toJSON());
+      return alfaResult;
+    });
+
+  // Fail the test if any rule is failing
+  // Note: this happens in a separate chain to avoid interfering with the
+  // reporting.
+  audit.then(async (alfaResult) => {
+    const failingRules = alfaResult.resultAggregates.filter(
+      (aggregate) => aggregate.failed > 0,
+    );
+
+    // Fail the test if any rule failed.
+    expect(failingRules.size).to.equal(
+      0,
+      `The page has ${failingRules.size} failing rules`,
+    );
+  });
+});
