ae20203d74a54e4285e649c8878d5e4b70668df7
Jean-Yves Moyen <jym@siteimprove.com>
2025-12-09 12:54:20 +0100

    Use vitest/alfa-test in Code Checker workspaces

--- accessibility-code-checker/puppeteer/test/puppeteer.spec.ts
+++ accessibility-code-checker/puppeteer/test/puppeteer.spec.ts
@@ -6,87 +6,91 @@ import {
   SIP,
 } from "@siteimprove/alfa-test-utils";
 
-import { test } from "@siteimprove/alfa-test-deprecated";
+import { test } from "@siteimprove/alfa-test";
 import { getCommitInformation } from "@siteimprove/alfa-test-utils/git";
 
 import puppeteer from "puppeteer";
 
-test("Page should be accessible", async (t) => {
-  // Create a browser instance
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-dev-shm-usage"],
-  });
+test(
+  "Page should be accessible",
+  async (t) => {
+    // Create a browser instance
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-dev-shm-usage"],
+    });
 
-  // Navigate to the local web page
-  // This supposes that the server is already running. See the demo-site folder.
-  // TODO: Replace with your own page
-  const page = await browser.newPage();
-  await page.goto("http://localhost:5173");
+    // Navigate to the local web page
+    // This supposes that the server is already running. See the demo-site folder.
+    // TODO: Replace with your own page
+    const page = await browser.newPage();
+    await page.goto("http://localhost:5173");
 
-  // Get the document handle from the page
-  const document = await page.evaluateHandle(() => window.document);
+    // Get the document handle from the page
+    const document = await page.evaluateHandle(() => window.document);
 
-  /*
-   * Usual Puppeteer instructions can live here.
-   * For example, navigating through the page, opening menus or modals, etc.
-   */
+    /*
+     * Usual Puppeteer instructions can live here.
+     * For example, navigating through the page, opening menus or modals, etc.
+     */
 
-  // Scrape the page
-  const alfaPage = await Puppeteer.toPage(document);
+    // Scrape the page
+    const alfaPage = await Puppeteer.toPage(document);
 
-  // Close the browser
-  await browser.close();
+    // Close the browser
+    await browser.close();
 
-  // Run the audit
-  const alfaResult = await Audit.run(alfaPage);
+    // Run the audit
+    const alfaResult = await Audit.run(alfaPage);
 
-  // (mandatory) Setup credentials (e.g., from environment variables).
-  const userName = process.env.SI_USER_EMAIL;
-  const apiKey = process.env.SI_API_KEY;
+    // (mandatory) Setup credentials (e.g., from environment variables).
+    const userName = process.env.SI_USER_EMAIL;
+    const apiKey = process.env.SI_API_KEY;
 
-  // (mandatory) Setup site ID; TODO: replace with your own.
-  const siteID = 16788956729;
+    // (mandatory) Setup site ID; TODO: replace with your own.
+    const siteID = 16788956729;
 
-  // (recommended) Fetch information about the latest commit
-  const gitInformation = await getCommitInformation();
+    // (recommended) Fetch information about the latest commit
+    const gitInformation = await getCommitInformation();
 
-  // (optional) Name the test, this can be built from the commit information.
-  const testName = (commit: CommitInformation) =>
-    `On branch ${commit.BranchName} – Puppeteer integration`;
+    // (optional) Name the test, this can be built from the commit information.
+    const testName = (commit: CommitInformation) =>
+      `On branch ${commit.BranchName} – Puppeteer integration`;
 
-  // (optional) Provide a page title, this defaults to the first <title> element.
-  // const pageTitle = "My page title";
+    // (optional) Provide a page title, this defaults to the first <title> element.
+    // const pageTitle = "My page title";
 
-  // (optional) Provide a page URL, this defaults to the page URL upon scraping.
-  // This is useful to overwrite localhost URLs.
-  const pageURL = "https://demo.siteimprovedemo.com/";
+    // (optional) Provide a page URL, this defaults to the page URL upon scraping.
+    // This is useful to overwrite localhost URLs.
+    const pageURL = "https://demo.siteimprovedemo.com/";
 
-  // Upload the result to Siteimprove Intelligence Platform, if credentials are provided
-  const url = await SIP.upload(alfaResult, {
-    // mandatory options
-    userName,
-    apiKey,
-    siteID,
-    // optional options
-    commitInformation: gitInformation,
-    testName,
-    // pageTitle,
-    pageURL,
-  });
+    // Upload the result to Siteimprove Intelligence Platform, if credentials are provided
+    const url = await SIP.upload(alfaResult, {
+      // mandatory options
+      userName,
+      apiKey,
+      siteID,
+      // optional options
+      commitInformation: gitInformation,
+      testName,
+      // pageTitle,
+      pageURL,
+    });
 
-  // Log the result to the console
-  Logging.fromAudit(alfaResult, url).print();
+    // Log the result to the console
+    Logging.fromAudit(alfaResult, url).print();
 
-  // Check if some rule was failing.
-  const failingRules = alfaResult.resultAggregates.filter(
-    (aggregate) => aggregate.failed > 0,
-  );
+    // Check if some rule was failing.
+    const failingRules = alfaResult.resultAggregates.filter(
+      (aggregate) => aggregate.failed > 0,
+    );
 
-  // Fail the test if any rule failed.
-  t.equal(
-    failingRules.size,
-    0,
-    `The page has ${failingRules.size} failing rules`,
-  );
-});
+    // Fail the test if any rule failed.
+    t.equal(
+      failingRules.size,
+      0,
+      `The page has ${failingRules.size} failing rules`,
+    );
+  },
+  { timeout: 60_000 /* ms */ },
+);
